ad34e4e989c5b52f1ab37915ba1fb606aa3d3139
Florian Cadet <cadet.florian@gmail.com>
2021-06-19 13:36:00 +0200

    palet color change

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 //pub const COLORS: [u32; 5] = [0xd9ffdb, 0x8fff94, 0x00cf0a, 0x006605, 0x002902];
-pub const COLORS: [u32; 5] = [0x8fff94, 0x8fff94, 0x00cf0a, 0x006605, 0x002902];
+//pub const COLORS: [u32; 5] = [0x8fff94, 0x8fff94, 0x00cf0a, 0x006605, 0x002902];
+pub const COLORS: [u32; 5] = [0xabebc6, 0xabebc6, 0x28b463, 0x145a32, 0x1b2631];
 
 pub fn fatal_err(msg: &str, status: i32) -> ! {
     eprintln!("Error: {}", msg);
